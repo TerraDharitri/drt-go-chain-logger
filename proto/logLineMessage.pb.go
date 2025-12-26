03c6789423e36c1f7b5ccdc9b8f45dc1467131ca
kasiaurangabad <kasi.aurangabad@gmail.com>
2025-12-26 16:33:14 +0530

    v1.1.0/v0.0.5

--- proto/logLineMessage.pb.go
+++ proto/logLineMessage.pb.go
@@ -735,7 +735,10 @@ func (m *LogLineMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthLogLineMessage
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthLogLineMessage
 			}
 			if (iNdEx + skippy) > l {
@@ -887,7 +890,10 @@ func (m *LogCorrelationMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthLogLineMessage
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthLogLineMessage
 			}
 			if (iNdEx + skippy) > l {
