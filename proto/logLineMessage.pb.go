3312917c88a6ac184a9eba824d9aa7b3ee4fb4ce
Mangonui <kasi.aurangabad2@gmail.com>
2025-12-29 11:58:51 +0530

    v1.1.0/0.0.4

--- proto/logLineMessage.pb.go
+++ proto/logLineMessage.pb.go
@@ -735,10 +735,7 @@ func (m *LogLineMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthLogLineMessage
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthLogLineMessage
 			}
 			if (iNdEx + skippy) > l {
@@ -890,10 +887,7 @@ func (m *LogCorrelationMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthLogLineMessage
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthLogLineMessage
 			}
 			if (iNdEx + skippy) > l {
